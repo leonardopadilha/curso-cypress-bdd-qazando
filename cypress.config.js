15331c83101d618f5bf65fd04a4b552526e3c860
leonardopadilha <leonardopadilha28@gmail.com>
2024-04-30 08:23:19 -0300

    Configurando relatório no arquivo cypress.config

--- cypress.config.js
+++ cypress.config.js
@@ -4,10 +4,13 @@ const { addCucumberPreprocessorPlugin } = require("@badeball/cypress-cucumber-pr
 const { createEsbuildPlugin }  = require("@badeball/cypress-cucumber-preprocessor/esbuild");
 
 module.exports = defineConfig({
+  projectId: 'vjom5y',
   e2e: {
+    reporter: 'cypress-mochawesome-reporter',
     baseUrl: "https://automationpratice.com.br",
     specPattern: "**/*.feature",
     setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
       addCucumberPreprocessorPlugin(on, config);
 
       on(
