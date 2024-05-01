a7f96ef62b93cf2265bf1ddc4947eeca65fb3148
leonardopadilha <leonardopadilha28@gmail.com>
2024-05-01 08:23:42 -0300

    Elements - Register

--- cypress/elements/register_elements.js
+++ cypress/elements/register_elements.js
@@ -4,6 +4,16 @@ const registerElements = {
     },
     error: {
         message: '#errorMessageFirstName'
+    },
+    input: {
+        name: '#user',
+        email: '#email',
+        password: '#password'
+    },
+    modal: {
+        check: '.swal2-success div:nth-child(4)',
+        successMessage: '#swal2-title',
+        welcomeMessage: '#swal2-html-container'
     }
 }
 
