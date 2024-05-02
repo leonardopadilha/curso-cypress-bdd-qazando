4e140b1cccdb78fcd91624b8e1cb2ef668c63646
leonardopadilha <leonardopadilha28@gmail.com>
2024-05-02 10:09:32 -0300

    Métodos de preenchimento e de validação

--- cypress/support/pages/register_page.js
+++ cypress/support/pages/register_page.js
@@ -14,5 +14,33 @@ export default {
                 expect(element).to.be.visible
                 expect(element.text()).to.be.equal(mensagem)
             })
+    },
+    preencherNome(nome) {
+        cy.get(registerElements.input.name)
+            .type(nome)
+    },
+    preencherEmail(email) {
+        cy.get(registerElements.input.email)
+            .type(email)
+    },
+    preencherSenha(senha) {
+        cy.get(registerElements.input.password)
+            .type(senha)
+    },
+    validarCadastroComSucesso(name) {
+        cy.get(registerElements.modal.check)
+            .should("be.visible");
+
+    cy.get(registerElements.modal.successMessage)
+        .should("be.visible")
+        .then((createUserSuccess) => {
+            expect(createUserSuccess.text()).to.equal("Cadastro realizado!");
+        });
+
+    cy.get(registerElements.modal.welcomeMessage)
+        .should("be.visible")
+        .then((messageSucces) => {
+            expect(messageSucces.text()).to.equal(`Bem-vindo ${name}`);
+        });
     }
 }
